542887614a19eda886810e8032402a1ad9136db1
João victor M <115943161+joaovictormf321@users.noreply.github.com>
2023-07-12 00:10:19 -0300

    .

---
+++ Others/Estudos/JavaScript/W3school/Mudar elemento HTML/Mudando DIV/script/main.js
@@ -0,0 +1,8 @@
+function button() {
+    document.getElementById("div").style.display = "none";
+    document.getElementById("div2").style.display = "block";
+}
+function button2() {
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div").style.display = "block";
+}
